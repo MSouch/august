d5ad4684fef3c477fdf529e647750f7b3a2709d7
MSouch <msouch3@gmail.com>
2023-10-16 15:01:53 -0600

    temp change!!

--- temp.py
+++ temp.py
@@ -1,4 +1,9 @@
-print("Enter a temperature in Celcius to convert into Farenheit: ")
-celcius = int(input())
-faren = celcius / 5 * 9 + 32
-print (faren)
+def celcius_to_farenheit():
+    faren = 25 * 9/5 + 32
+    return(faren)
+
+def farenheit_to_celcius():
+    celcius = 5/9 * (82 - 32)
+    return(celcius)
+
+print(f"25 Celcius to Farenheit is: {celcius_to_farenheit()}, and 82 Farenheit to Celcius is: {farenheit_to_celcius()}")
